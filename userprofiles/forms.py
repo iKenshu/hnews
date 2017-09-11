2102dc476844d7b4a129308196a182d956871449
Julio Hurtado <juliens@outlook.com>
2017-09-10 20:21:22 -0500

    Fix SignUpForm

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 from .models import Profile
 
-class SignUpAndSignInForm(forms.ModelForm):
+class SignUpAndSignInForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username',)
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
         }
 
