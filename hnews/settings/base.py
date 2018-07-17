c27b5ec3f51645e1da7f13c2fe5b47fc549083a3
iKenshu <undefined.hlo.o@gmail.com>
2018-07-17 10:53:36 -0400

    static folder

--- hnews/settings/base.py
+++ hnews/settings/base.py
@@ -119,5 +119,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
